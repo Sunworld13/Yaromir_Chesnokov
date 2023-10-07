733d14690498f0865cde10c6d7583f2bbf948563
Чесноков Я.М <chesnokovyam@lab227.local>
2023-10-07 09:01:48 +0300

    first commit

---
+++ xpenb1/src/shape/Point.java
@@ -0,0 +1,22 @@
+package shape;
+
+public abstract class Point extends Shape
+{
+    private double x = 0.0, y=0.0;
+
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public s
+}
---
+++ xpenb1/src/shape/Shape.java
@@ -0,0 +1,32 @@
+package shape;
+
+import java.awt.*;
+
+public abstract class Shape {
+    public Color color = Color.white;
+    public Color getColor()
+    {
+        return color;
+    }
+    public void setColor(){this.color = color;}
+
+
+    public void setColor(Color color)
+    {
+        this.color = color;
+    }
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    public void move(double moveX, double moveY)
+    {
+
+    }
+    public void draw() {
+        System.out.println("Фигура: " + getShapeName());
+        System.out.println("Цвет: " + getColor());
+        System.out.println("Площадь: " + getArea());
+        System.out.println("Периметр: " + getPerimeter());
+        System.out.println("Координаты точек: " + getCoordinates());
+    }
+}
